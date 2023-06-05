8562c8e63ee781556e0cc3adf47517304c791ca8
Jeff Labonte <jflabonte@poka.io>
2023-06-05 12:35:17 -0400

    Add neorg to plugins

---
+++ lua/custom/plugins/neorg.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                work = "~/Notes/work",
+                home = "~/Notes/home"
+              }
+            }
+          }
+        }
+      })
+    end,
+  }
+}
