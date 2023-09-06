0340bdcbf6e05764d8442770e5e1534fbd745bf7
Jeff Labonte <jflabonte@poka.io>
2023-09-06 09:27:26 -0400

    Add extra keymap for close-buffers

--- lua/custom/plugins/close_buffer.lua
+++ lua/custom/plugins/close_buffer.lua
@@ -1,3 +1,12 @@
 return {
   "kazhala/close-buffers.nvim",
+  config = function()
+    vim.keymap.set("n", "<leader>ba", function()
+      vim.cmd("Bdelete all")
+    end, { desc = "Close all buffers" })
+
+    vim.keymap.set("n", "<leader>bh", function ()
+      vim.cmd("Bdelete hidden")
+    end, { desc = "Close all hidden buffers" })
+  end
 }
