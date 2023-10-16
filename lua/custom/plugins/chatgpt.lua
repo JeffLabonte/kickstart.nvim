dc089fd51ea90c63dd89f88dc0d73051d02a353e
Jean-Francois Labonte <grimsleepless@protonmail.com>
2023-10-15 22:52:33 -0400

    Add new logic

--- lua/custom/plugins/chatgpt.lua
+++ lua/custom/plugins/chatgpt.lua
@@ -1,3 +1,10 @@
+local openapi_key = os.getenv("OPENAI_API_KEY")
+
+if openapi_key == nil then
+  print("OPENAI_API_KEY not set")
+  return {}
+end
+
 return {
   "jackMort/ChatGPT.nvim",
   event = "VeryLazy",
