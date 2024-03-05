d6e92bd17db8f143c9c285c208146dfe8db91bb5
Jeff Labonte <jflabonte@poka.io>
2024-03-05 15:50:02 -0500

    Remove copilot in favour of tabnine
    # Conflicts:
    #	init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,23 +3,8 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'zbirenbaum/copilot.lua',
-    config = function()
-      require('copilot').setup {
-        suggestion = {
-          enabled = false,
-        },
-        panel = {
-          enabled = false,
-        },
-      }
-    end,
-  },
-  {
-    'zbirenbaum/copilot-cmp',
-    config = function()
-      require('copilot_cmp').setup()
-    end,
+  { 
+    'codota/tabnine-nvim',
+    build = "./dl_binaries.sh",
   },
 }
