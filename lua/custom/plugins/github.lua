dc8760db48847ffead0eb9efa214800402ebcba2
Jeff Labonte <jflabonte@poka.io>
2024-03-05 15:50:02 -0500

    Remove copilot in favour of tabnine

--- lua/custom/plugins/github.lua
+++ lua/custom/plugins/github.lua
@@ -1,11 +1,13 @@
 return {
-  'pwntester/octo.nvim',
-  requires = {
-    'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope.nvim',
-    'nvim-tree/nvim-web-devicons',
+  {
+    'pwntester/octo.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function ()
+      require"octo".setup()
+    end
   },
-  config = function ()
-    require"octo".setup()
-  end
 }
