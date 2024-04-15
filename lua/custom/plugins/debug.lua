fad980c68df0f27c20f7571a0ed0b0d6e35bfba5
Jeff Labonte <jflabonte@poka.io>
2024-04-15 14:23:10 +0000

    Add both dependencies

--- lua/custom/plugins/debug.lua
+++ lua/custom/plugins/debug.lua
@@ -1,3 +1,12 @@
 return {
-  "nvim-neotest/nvim-nio"
+  {"nvim-neotest/nvim-nio"},
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter"
+    }
+  }
 }
